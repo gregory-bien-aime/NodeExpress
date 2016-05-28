0c67c59a5392018fbe27e1a2862732049661d9f7
Gregory <gregory.bienaime@yahoo.com>
2016-05-27 22:46:58 -0400

    Test

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+                }, {
+    Link: '/Author', 
+    Text: 'Author'
+    }];
+
+var bookRouter = require('./src/routes/bookroutes')(nav);
+var adminRouter = require('.src/routes/adminRoutes')(nav);
+var authRouter = require('.src/routes/authRoutes')(nav);
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
+app.uae('/Admin', adminRouter);
+app.use('Auth', authRouter);
+
+app.get('/', function(req, res) {
+    res.render('index', {
+                    title: 'Hello from render',
+                    nav: [{
+                        
+                            Link: '/Books',
+                            Text: 'Books'
+                        }, {
+                            Link: '/Authors', 
+                            Text: 'Authors'
+                        }]
+        }); //routing request and response
+        });
+
+app.get('/books', function(req, res) {
+    res.send('books');
+});
+app.listen(port, function (err){
+	console.log('running server on port' + port);
+
+});
